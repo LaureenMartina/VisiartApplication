dd4b08c10e88439cc62ca25e5b2f8e6a8a688a93
LaureenMartina <L.martina@protonmail.com>
2020-04-10 18:33:52 +0200

    up events

--- visiart/lib/events/eventsList.dart
+++ visiart/lib/events/eventsList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'dart:convert';
 
 class EventsListScreen extends StatefulWidget {
   @override
@@ -8,6 +9,33 @@ class EventsListScreen extends StatefulWidget {
 }
 
 class _EventsListScreenState extends State<EventsListScreen> {
+
+  String name, username, avatar;
+  bool isData = false;
+
+  /*
+  * EXEMPLE : API GET PARSE JSON
+  */
+  /*FetchJSON() async {
+    var Response = await http.get(
+      "https://api.github.com/users/nitishk72",
+      headers: {"Accept": "application/json"},
+    );
+
+    if (Response.statusCode == 200) {
+      String responseBody = Response.body;
+      var responseJSON = json.decode(responseBody);
+      username = responseJSON['login'];
+      name = responseJSON['name'];
+      avatar = responseJSON['avatar_url'];
+      isData = true;
+      setState(() {
+        print('UI Updated');
+      });
+    } else {
+      print('Something went wrong. \nResponse Code : ${Response.statusCode}');
+    }
+  }*/
 
   @override
   Widget build(BuildContext context) {
@@ -19,8 +47,47 @@ class _EventsListScreenState extends State<EventsListScreen> {
               return Container(
                 alignment: Alignment.center,
                 height: 180,
-                child: Text('event $index'),
-                color: Colors.orange[100 * (index % 10)],
+                color: Colors.red[100 * (index % 10)],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  //crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[                
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Container(
+                                  margin: const EdgeInsets.only(top: 12, bottom: 10, left: 12),
+                                  child: Text("TITLE EVENT $index",
+                                    style: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 10, left: 12),
+                            child: Text("Description event : aaaaaaaaaaaaaaaaa",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ,
+                  ],
+                ),
               );
             }, childCount: 10,
             ),
