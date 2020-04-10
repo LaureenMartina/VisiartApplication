dbbc67fd72b1a92879d9eaeec87f73bb5f86c658
LaureenMartina <L.martina@protonmail.com>
2020-04-10 14:49:13 +0200

    added events screen

--- visiart/lib/events/eventsList.dart
+++ visiart/lib/events/eventsList.dart
@@ -12,8 +12,50 @@ class _EventsListScreenState extends State<EventsListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Container(
+                alignment: Alignment.center,
+                height: 180,
+                child: Text('event $index'),
+                color: Colors.orange[100 * (index % 10)],
+              );
+            }, childCount: 10,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: SpeedDial(
+        closeManually: true,
+        animatedIcon: AnimatedIcons.menu_close,
+        children: [
+          SpeedDialChild(
+            backgroundColor: Colors.redAccent[200],
+            child: Icon(Icons.favorite),
+            label: 'favorite',
+            onTap: () {
+              print("favorie");
+            }
+          ),
+          SpeedDialChild(
+            backgroundColor: Colors.greenAccent[200],
+            child: Icon(Icons.filter_list),
+            label: 'Récent',
+            onTap: () {
+              print("récent");
+            }
+          ),
+          SpeedDialChild(
+            backgroundColor: Colors.blueAccent[200],
+            child: Icon(Icons.map),
+            label: 'Map',
+            onTap: () {
+              print("récent");
+            }
+          ),
+        ],
       ),
     );
   }
