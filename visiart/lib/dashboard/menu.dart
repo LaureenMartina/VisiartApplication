b4aac8b6cf5d40dda8fed7be255dbf7b1dff7990
LaureenMartina <L.martina@protonmail.com>
2020-04-10 14:43:45 +0200

    update menu/dashboard

--- visiart/lib/dashboard/menu.dart
+++ visiart/lib/dashboard/menu.dart
@@ -20,6 +20,54 @@ class _MenuBoardScreenState extends State<MenuBoardScreen> {
     EventsListScreen()
   ];
 
+  final account = ListTile(
+    title: Text('Mon Compte',
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text('Vos données enregistrées',
+      style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
+      ),
+    ),
+    leading: Icon(Icons.face, size: 40, color: Colors.teal[300]),
+    onTap: () {  },
+  );
+
+  final about = ListTile(
+    title: Text('A Propos',
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text('Notre équipe, notre application',
+      style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
+      ),
+    ),
+    leading: Icon(Icons.announcement, size: 40, color: Colors.indigoAccent[400]),
+    onTap: () {  },
+  );
+
+  final paramsUser = ListTile(
+    title: Text('Paramètres',
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text('Données de l\'application',
+      style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
+      ),
+    ),
+    leading: Icon(Icons.settings, size: 40, color: Colors.blueGrey[700]),
+    onTap: () {  },
+  );
+
+  final logout = ListTile(
+    title: Text('Déconnexion',
+      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text('Bye Bye',
+      style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
+      ),
+    ),
+    leading: Icon(Icons.launch, size: 40, color: Colors.brown[900],),
+    onTap: () {  },
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +80,7 @@ class _MenuBoardScreenState extends State<MenuBoardScreen> {
           children: <Widget>[
             UserAccountsDrawerHeader(
               accountName: Text('Exemple Machin',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.indigo[900]),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white, letterSpacing: 2),
               ),
               accountEmail: Text('exemple.machin@gmail.com',
                 style: TextStyle(fontSize: 12, color: Colors.cyanAccent[100]),
@@ -49,75 +97,44 @@ class _MenuBoardScreenState extends State<MenuBoardScreen> {
                 )
               ),
             ),
-            ListTile(
-              title: Text('A Propos',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Notre équipe, notre application',
-                style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
-                ),
-              ),
-              leading: Icon(Icons.announcement, size: 40, color: Colors.indigoAccent[400]),
-              onTap: () {  },
-            ),
-            ListTile(
-              title: Text('Paramètres',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Données de l\'application',
-                style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
-                ),
-              ),
-              leading: Icon(Icons.settings, size: 40, color: Colors.blueGrey[700]),
-              onTap: () {  },
-            ),
+            account,
+            about,
+            paramsUser,
             Divider(
               indent: 10,
               endIndent: 10,
               thickness: 1,
               color: Colors.deepPurpleAccent[200],
             ),
-            ListTile(
-              title: Text('Déconnexion',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('Bye Bye',
-                style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic
-                ),
-              ),
-              leading: Icon(Icons.launch, size: 40, color: Colors.brown[900],),
-              onTap: () {  },
-            ),
+            logout,
           ],
         ),
       ),
       body: _children[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text('Home'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.forum),
-          title: Text('Salons'),
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.local_play),
-          title: Text('Events'),
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.amber[800],
-      onTap: (value) {
-        print(value);
-        _selectedIndex = value;
-        print(_selectedIndex);
-        setState(() {
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.forum),
+            title: Text('Salons'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_play),
+            title: Text('Events'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: (value) {
           _selectedIndex = value;
-        });
-      },
-    ),
+          setState(() {
+            _selectedIndex = value;
+          });
+        },
+      ),
     );
   }
 }
--- visiart/lib/events/eventsList.dart
+++ visiart/lib/events/eventsList.dart
@@ -1,9 +1,6 @@
-import 'dart:ui';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 class EventsListScreen extends StatefulWidget {
   @override
@@ -11,134 +8,13 @@ class EventsListScreen extends StatefulWidget {
 }
 
 class _EventsListScreenState extends State<EventsListScreen> {
-  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      /*primary: false,
-      slivers: <Widget>[
-        SliverPadding(
-          padding: const EdgeInsets.only(left: 0, right: 0),
-          sliver: SliverGrid.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-              Align(
-                child: CupertinoButton(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/imgs/pattern.png"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
-                      child: Text(
-                        "Trophées",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    print(Text('hello'));
-                  },
-                ),
-              ),
-              Align(
-                child: CupertinoButton(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/imgs/pattern.png"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
-                      child: Text(
-                        "Trophées",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    print(Text('hello'));
-                  },
-                ),
-              ),
-              Align(
-                child: CupertinoButton(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/imgs/pattern.png"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
-                      child: Text(
-                        "Trophées",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    print(Text('hello'));
-                  },
-                ),
-              ),
-              Align(
-                child: CupertinoButton(
-                  child: Container(
-                    height: 150,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/imgs/pattern.png"),
-                        fit: BoxFit.cover,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Container(
-                      margin: EdgeInsets.fromLTRB(15, 15, 0, 0),
-                      child: Text(
-                        "Trophées",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {
-                    print(Text('hello'));
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ],*/
+    return Scaffold(
+      body: Container(
+
+      ),
     );
   }
 }
