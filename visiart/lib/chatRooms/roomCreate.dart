a0137bf884d6ad4b385d0da28b74661543faa780
Florian <florian.mazuy@gmail.com>
2020-06-07 15:59:01 +0200

    Ajout sytème hobby

--- visiart/lib/chatRooms/roomCreate.dart
+++ visiart/lib/chatRooms/roomCreate.dart
@@ -5,7 +5,12 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:visiart/chatRooms/Room.dart';
 import 'package:http/http.dart' as http;
+import 'package:visiart/chatRooms/roomsList.dart';
+import 'package:visiart/config/SharedPref.dart';
+import 'package:visiart/models/Hobby.dart';
 
+
+SharedPref sharedPref = SharedPref();
 class RoomsCreateScreen extends StatefulWidget { 
   @override
   _RoomsCreateScreenState createState() => _RoomsCreateScreenState();
@@ -22,30 +27,70 @@ class _RoomsCreateScreenState extends State<RoomsCreateScreen> {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   _RoomsCreateData _data = new _RoomsCreateData();
 
-  Future<http.Response> createRoom(String newRoomName, String newRoomThematic) {
-    return http.post(
+  List<Hobby> listHobbies = [];
+  var selectedHobby;
+  var selectedPrivateMessage = "NON";
+  var selectedDisplayMessage = "OUI";
+  var selectedPrivateBool = false;
+  var selectedDisplayBOOL = true;
+  @override
+    void initState() {
+      getListHobbies();
+      
+      super.initState();
+    }
+
+  Future<http.Response> createRoom(String newRoomName, String newRoomThematic) async {
+    var token = await sharedPref.read('token');
+    final response = await http.post(
         'http://91.121.165.149/rooms',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': 'Bearer token',
+            'Authorization': 'Bearer $token',
         },
-        body: jsonEncode(<String, String>{
+        body: jsonEncode(<String, dynamic>{
             'name': newRoomName,
+            'hobbies' : selectedHobby,
+            'display' : selectedDisplayBOOL.toString(),
+            'private' : selectedPrivateBool.toString(),
+
         }),
     );
+    if (response.statusCode == 200) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => RoomsListPage()),
+        );
+    } else {
+      throw Exception('Failed to post room from API');
+    }
+  }
+  Future<http.Response> getListHobbies() async{
+    var token = await sharedPref.read("token");
+    final response = await http.get(
+        'http://91.121.165.149/hobbies',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token',
+        }
+    );
+    if (response.statusCode == 200) {
+        List jsonResponse = json.decode(response.body);
+        var items = jsonResponse.map((hobby) => new Hobby.fromJson(hobby)).toList();
+        setState(() {
+          this.listHobbies.addAll(items);
+        });
+    } else {
+      throw Exception('Failed to load hobbies from API');
+    }
   }
 
   void submit() {
     // First validate form.
     if (this._formKey.currentState.validate()) {
         _formKey.currentState.save(); // Save our form now.
-
-        
-        print('Printing the data.');
-        print('roomName: ${_data.roomName}');
-        print('roomThematic: ${_data.roomThematic}');
-
         this.createRoom(_data.roomName, _data.roomThematic);
     }
   }
@@ -73,7 +118,73 @@ class _RoomsCreateScreenState extends State<RoomsCreateScreen> {
                   this._data.roomName = value;
                 }
               ),
-              new TextFormField(
+              new DropdownButton<String>(
+                hint: Text("choisir le hobby"),
+                value: this.selectedHobby == null ? null : selectedHobby,
+                items: this.listHobbies.map((Hobby hobby) {
+                  return new DropdownMenuItem<String>(
+                    value: hobby.name,
+                    child: new Text(hobby.name),
+                  );
+                }).toList(),
+                onChanged: (_value) {
+                  this._data.roomThematic = _value;
+                   setState(() {
+                    selectedHobby = _value;
+                  });
+                },
+              ),
+              new DropdownButton<String>(
+                hint: Text("Affiché ?"),
+                value: this.selectedDisplayMessage == null ? null : selectedDisplayMessage,
+                items: <String>['OUI', 'NON']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_value) {
+                  if(_value == "OUI") {
+                    setState(() {
+                      selectedDisplayBOOL = true;
+                      selectedDisplayMessage = "OUI";
+                    });
+                  } else {
+                    setState(() {
+                      selectedDisplayBOOL = false;
+                      selectedDisplayMessage = "NON";
+                    });
+                  }
+                  
+                },
+              ),
+              new DropdownButton<String>(
+                hint: Text("Privé ?"),
+                value: this.selectedPrivateMessage == null ? null : selectedPrivateMessage,
+                items: <String>['OUI', 'NON']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (_value) {
+                  if(_value == "OUI") {
+                    setState(() {
+                      selectedPrivateBool = true;
+                      selectedPrivateMessage = "OUI";
+                    });
+                  } else {
+                    setState(() {
+                      selectedPrivateBool = false;
+                      selectedPrivateMessage = "NON";
+                    });
+                  }
+                  
+                },
+              ),
+              /* new TextFormField(
                 keyboardType: TextInputType.text,
                 decoration: new InputDecoration(
                   hintText: 'Thème du salon',
@@ -82,7 +193,7 @@ class _RoomsCreateScreenState extends State<RoomsCreateScreen> {
                 onSaved: (String value) {
                   this._data.roomThematic = value;
                 }
-              ),
+              ), */
               new Container(
                 width: screenSize.width,
                 child: new RaisedButton(
