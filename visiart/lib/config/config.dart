85c6084dc46e5e47d9742acfd8cc99ace6ea634a
LaureenMartina <L.martina@protonmail.com>
2020-06-15 18:32:47 +0200

    clean & start update dashboard

--- visiart/lib/config/config.dart
+++ visiart/lib/config/config.dart
@@ -7,6 +7,7 @@ const String API_REGISTER = API_BASE_URL + "/auth/local/register";
 const String API_LOGIN = API_BASE_URL + "/auth/local";
 const String API_EVENT = API_BASE_URL + "/events";
 const String API_EVENT_LANG = API_BASE_URL + "/events?_limit=50&language=";
+const String API_EVENT_CAROUSEL = API_BASE_URL + "/events?_limit=20&language=";
 
 const API_HEADERS = {
   'Content-Type': 'application/json',
--- visiart/lib/events/eventsList.dart
+++ visiart/lib/events/eventsList.dart
@@ -1,7 +1,5 @@
 import 'dart:convert';
 import 'dart:ui';
-
-import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
